0e978f4f22baa46a764edb2f4621ae491c2f2fb6
somyagupta07526 <somyaguptasg72@gmail.com>
2026-08-14 14:38:52 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-base: '/student-developer-dashboard/',
-// https://vitejs.dev/config/
+
 export default defineConfig({
+  base: '/student-developer-dashboard/',
   plugins: [react()],
   server: {
     port: 3000,
